5892f5fafb016903200f17206e977962410169d2
Sagarika Sahoo <sagarika.mama1941@gmail.com>
2020-10-24 14:58:16 +0200

    added home page test for search

--- src/test/java/komoot/HomeTest.java
+++ src/test/java/komoot/HomeTest.java
@@ -1,7 +1,10 @@
 package komoot;
 
 import komoot.pages.HomePage;
-import komoot.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeTest extends AbstractBaseTest {
@@ -16,5 +19,15 @@ public class HomeTest extends AbstractBaseTest {
         HomePage homePage = new HomePage(getDriver());
         homePage.selectSport("jogging");
         homePage.chooseLocation("Kopenhagener Strasse");
+
+        waitFor(5000);
+
+        WebDriver driver = getDriver();
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("window.scrollBy(0,750)");
+
+        String tourCount = driver.findElement(By.cssSelector(".c-filter-bar .tw-flex-auto")).getText();
+        System.out.println(tourCount);
+        Assert.assertTrue(tourCount.contains("Tours"));
     }
 }
